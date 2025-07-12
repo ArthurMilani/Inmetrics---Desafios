665841f8bd35b2335fc52fbe0265d328b8ba37fa
Arthur Milani Giovanini <arthur.milani@totvs.com.br>
2025-07-12 12:15:57 -0300

    Desafio03: Getting RDS DNS from file

--- Standart/Clientes/config.py
+++ Standart/Clientes/config.py
@@ -8,10 +8,13 @@ logging.basicConfig(
     datefmt='[%d/%b/%Y %H:%M:%S]'
     )
 
+with open('/opt/db_host.txt', 'r') as f:
+    db_host = f.read().strip()
+
 mysql = MySQL()
-app.config['MYSQL_DATABASE_USER'] = 'root'
+app.config['MYSQL_DATABASE_USER'] = 'ArthurMilani'
 app.config['MYSQL_DATABASE_PASSWORD'] = 'pacote321'
 app.config['MYSQL_DATABASE_DB'] = 'clients_context'
-app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+app.config['MYSQL_DATABASE_HOST'] = db_host
 app.config['SECRET_KEY'] = '57840ae5777fc4480db92b598731d686cbcdc3643fa087bdbf26ab347fec5951'
 mysql.init_app(app)
